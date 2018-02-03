ec3571c1740ded9f332a0a86dd95b048aba18a31
Brandon Stevens <stevens.brandon@gmail.com>
2018-02-03 22:49:00 +0000

    Add Provider and Support for Managing Hosted Collectors
     * Added the provider which requires a basic authentication token and endpoint
    URL.
     * Added support for managing hosted collectorsa
     * Initial vendoring.

---
+++ sumologic/provider_test.go
@@ -0,0 +1,38 @@
+package sumologic
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"sumologic": testAccProvider,
+	}
+}
+
+func TestProvider(t *testing.T) {
+	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("SUMOLOGIC_AUTH_TOKEN"); v == "" {
+		t.Fatal("SUMOLOGIC_AUTH_TOKEN must be set for acceptance tests")
+	}
+	if v := os.Getenv("SUMOLOGIC_ENDPOINT_URL"); v == "" {
+		t.Fatal("SUMOLOGIC_ENDPOINT_URL must be set for acceptance tests")
+	}
+}
